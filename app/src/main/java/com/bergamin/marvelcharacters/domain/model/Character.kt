34935aa3d1fc9dccf3cba028fc421150eec1886d
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2019-10-12 20:49:30 -0300

    Character classes added

---
+++ app/src/main/java/com/bergamin/marvelcharacters/domain/model/Character.kt
@@ -0,0 +1,33 @@
+package com.bergamin.marvelcharacters.domain.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Character(
+    val name: String? = null,
+    val picture: String? = null
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(picture)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Character> {
+        override fun createFromParcel(parcel: Parcel): Character {
+            return Character(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Character?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
