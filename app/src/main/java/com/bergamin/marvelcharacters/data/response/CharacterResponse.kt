bc0b00c558d3a6632f8d1fe5cc386a7622c7cf29
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2019-10-16 21:59:07 -0300

    Fixed some mistakes

--- app/src/main/java/com/bergamin/marvelcharacters/data/response/CharacterResponse.kt
+++ app/src/main/java/com/bergamin/marvelcharacters/data/response/CharacterResponse.kt
@@ -2,7 +2,7 @@ package com.bergamin.marvelcharacters.data.response
 
 import com.google.gson.annotations.SerializedName
 
-class CharacterResponse (
+data class CharacterResponse (
     @SerializedName("Name") val name: String? = null,
     @SerializedName("Picture") val picture: String? = null
 )
--- app/src/main/java/com/bergamin/marvelcharacters/data/response/SearchResponse.kt
+++ app/src/main/java/com/bergamin/marvelcharacters/data/response/SearchResponse.kt
@@ -2,6 +2,6 @@ package com.bergamin.marvelcharacters.data.response
 
 import com.google.gson.annotations.SerializedName
 
-class SearchResponse(
+data class SearchResponse(
     @SerializedName("Search") val search: List<CharacterResponse>? = null
 )
