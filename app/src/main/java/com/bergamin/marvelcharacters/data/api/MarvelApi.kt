dfb325259354f34ee840269073115c93576bd4b7
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2019-10-21 01:47:44 -0300

    Fixed getting character by ID

--- app/src/main/java/com/bergamin/marvelcharacters/data/api/MarvelApi.kt
+++ app/src/main/java/com/bergamin/marvelcharacters/data/api/MarvelApi.kt
@@ -4,13 +4,14 @@ import com.bergamin.marvelcharacters.data.response.CharacterResponse
 import com.bergamin.marvelcharacters.data.response.SearchResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MarvelApi {
 
-    @GET("/")
+    @GET("/v1/public/characters")
     fun getSearch(@Query("nameStartsWith") name: String): Call<SearchResponse>
 
-    @GET("/")
-    fun getCharacter(@Query("") id: String): Call<CharacterResponse>
+    @GET("/v1/public/characters/{id}")
+    fun getCharacter(@Path("id") id: String): Call<CharacterResponse>
 }
